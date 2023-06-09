03f712d6fc581c36bdbf84a30d94b1eb7f876be7
anthonylatorre764 <anthonylatorre764@gmail.com>
2023-06-09 01:14:14 -0400

    added codewars solutions

--- main.js
+++ main.js
@@ -9,8 +9,8 @@ let dog_names = ["Max","HAS","PuRple","dog"]
 
 function findWords(aString, aList){
     for(let dog_name of aList){
-        if (aString.includes(dog_name)){
-            console.log(`Matched ${dog_name}`);
+        if (aString.includes(dog_name)){                        // the .includes() method returns True if the array
+            console.log(`Matched ${dog_name}`);                 // contains the given value
         } 
         else {
             console.log('No Match');
@@ -34,9 +34,9 @@ function replaceEvens(anArray){
     // arr.splice(i)
     for(let i in anArray){
         if (i % 2 == 0){
-            anArray.splice(i, 1, 'even index')
-        } 
-    }
+            anArray.splice(i, 1, 'even index')                      // the .splice() method used here is removing one
+        }                                                           // element from the array and then adding one string 
+    }                                                               // element at index i (even indexes)
 
 }
 
@@ -56,13 +56,39 @@ console.log(arr)
 // codewars problem #1 - "Array plus array"
 // https://www.codewars.com/kata/5a2be17aee1aaefe2a000151/javascript
 
+
 // my solution
-// https://www.codewars.com/kata/5a2be17aee1aaefe2a000151/solutions/javascript?filter=me&sort=best_practice&invalids=false
+// (credit to Dionysia Lemonaki from freeCodeCamp for solution)
+
+// function arrayPlusArray(arr1, arr2) {
+//     var bigArray = arr1.concat(arr2)
+//     var sum = bigArray.reduce((partialSum, currentValue) => {
+//       return partialSum + currentValue
+//     }, 0);
+//     return sum
+//   }
+
+
+// the .concat() method joins two or more arrays.
+// once all the elements from both arrays were inside
+// the bigArray array, I used the .reduce() method to add
+// every element in the array to another object called partialSum.
+// Then the reducer function returned the value of partialSum
+// and stored it in an object called sum. My function then returned sum.
+
+
+
 
 
 
 // codewars problem #2 - "Jenny's secret message"
 // https://www.codewars.com/kata/55225023e1be1ec8bc000390
 
+
 // my solution
-// https://www.codewars.com/users/soccer_fiend_12/completed_solutions
+
+// function greet(name){
+//     if(name === "Johnny")
+//       return "Hello, my love!";
+//     return "Hello, " + name + "!";
+//   }
